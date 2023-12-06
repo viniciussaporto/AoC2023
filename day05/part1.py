3a93a2481aac3f7922c345ca3bf88aa0298e3e04
viniciussaporto <vinicius.saporto+github@gmail.com>
2023-12-06 00:50:40 +0000

    linting

--- day05/part1.py
+++ day05/part1.py
@@ -1,28 +1,28 @@
-with open('input.txt') as f:
-    lines = f.read().split("\n\n")
-    maps = lines[1::]
-    seeds = list(map(int, lines[0].split(":")[1].strip().split()))
+with open("input.txt") as f:
+	lines = f.read().split("\n\n")
+	maps = lines[1::]
+	seeds = list(map(int, lines[0].split(":")[1].strip().split()))
 
-    temp = seeds
-    min_l = 0
+	temp = seeds
+	min_l = 0
 
-    for m in maps:
-        ranges = []
+	for m in maps:
+		ranges = []
 
-        for line in m.split("\n")[1::]:
-            ranges.append(list(map(int, line.split())))
+		for line in m.split("\n")[1::]:
+			ranges.append(list(map(int, line.split())))
 
-        dest = []
+		dest = []
 
-        for s in temp:
-            for x, y, z in ranges:
-                if s in range(y, y + z):
-                    dest.append(x + (s - y))
-                    break
-            else:
-                dest.append(s)
-    
-        min_l = min(dest)
-        temp = dest
+		for s in temp:
+			for x, y, z in ranges:
+				if s in range(y, y + z):
+					dest.append(x + (s - y))
+					break
+			else:
+				dest.append(s)
+
+		min_l = min(dest)
+		temp = dest
 
 print(min_l)
