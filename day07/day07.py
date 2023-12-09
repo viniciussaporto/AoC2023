925183b79aaf52fa16356d58e044d57869feec6d
viniciussaporto <vinicius.saporto+github@gmail.com>
2023-12-09 01:12:19 +0000

    day 07 and 08 completed

---
+++ day07/day07.py
@@ -0,0 +1,47 @@
+from itertools import product
+from collections import Counter
+import fileinput
+
+
+def rank(hand):
+    c = Counter(Counter(hand).values())
+    if c[5]:
+        return 0
+    if c[4]:
+        return 1
+    if c[3] and c[2]:
+        return 2
+    if c[3]:
+        return 3
+    if c[2] > 1:
+        return 4
+    if c[2]:
+        return 5
+    return 6
+
+
+def best_rank(hand, part2):
+    jokers = 0
+    if part2:
+        jokers = hand.count("J")
+        hand = "".join(c for c in hand if c != "J")
+    return min(
+        rank(hand + "".join(cs)) for cs in product("23456789TQKA", repeat=jokers)
+    )
+
+
+def run(lines, part2):
+    cards = "AKQT98765432J" if part2 else "AKQJT98765432"
+    items = []
+    for line in lines:
+        hand, bid = line.strip().split()
+        items.append(
+            (best_rank(hand, part2), tuple(cards.index(c) for c in hand), int(bid))
+        )
+    items.sort(reverse=True)
+    return sum((i + 1) * bid for i, (_, _, bid) in enumerate(items))
+
+
+lines = list(fileinput.input())
+for i in range(2):
+    print(run(lines, i))
---
+++ day08/day08.py
@@ -0,0 +1,97 @@
+from collections import deque
+import timeit
+
+
+class Solution:
+    def read_data(self):
+        with open("input.txt", "r") as f:
+            data = f.read()
+        return data
+
+    def gcd(self, x, y):
+        while y:
+            x, y = y, x % y
+        return x
+
+    def lcm(self, x, y):
+        return x * y // self.gcd(x, y)
+
+    def part_one(self):
+        data = self.read_data().split("\n")
+        commands = data[0]
+        states = data[2:]
+        state_machine = {}
+
+        for i in range(len(states)):
+            from_state, to_states = states[i].split("=")
+            from_state = from_state.strip()
+
+            to_left, to_right = [
+                x.replace("(", "").replace(")", "").strip()
+                for x in to_states.split(",")
+            ]
+            state_machine[from_state] = (to_left, to_right)
+
+        cur_state = "AAA"
+        i = 0
+        counter = 0
+        while cur_state != "ZZZ":
+            left, right = state_machine[cur_state]
+            if commands[i] == "L":
+                cur_state = left
+            else:
+                cur_state = right
+
+            counter += 1
+            i = i + 1 if i < len(commands) - 1 else 0
+
+        return counter
+
+    def part_two(self):
+        data = self.read_data().split("\n")
+        commands = data[0]
+        states = data[2:]
+        state_machine = {}
+        start_states = []
+
+        for i in range(len(states)):
+            from_state, to_states = states[i].split("=")
+            from_state = from_state.strip()
+
+            to_left, to_right = [
+                x.replace("(", "").replace(")", "").strip()
+                for x in to_states.split(",")
+            ]
+            state_machine[from_state] = (to_left, to_right)
+
+            if from_state[-1] == "A":
+                start_states.append(from_state)
+
+        distances = []
+
+        for start_state in start_states:
+            cur = start_state
+            i = 0
+            distance = 0
+            while cur[-1] != "Z":
+                left, right = state_machine[cur]
+                cur = left if commands[i] == "L" else right
+                distance += 1
+                i = i + 1 if i < len(commands) - 1 else 0
+
+            distances.append(distance)
+
+        res = 1
+        for distance in distances:
+            res = self.lcm(res, distance)
+
+        return res
+
+
+def main():
+    sol = Solution()
+    print(f"Part one: {sol.part_one()}")
+    print(f"Part two: {sol.part_two()}")
+
+
+main()
